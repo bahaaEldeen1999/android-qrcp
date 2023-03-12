e9b742ac445ccd43a9b01e4044ddca647d8db56f
bahaaEldeen1999 <bahaaeldeen1999@gmail.com>
2023-03-12 21:13:55 +0200

    add size/ range requests

--- app/src/main/java/networking/NanoHttpServer.java
+++ app/src/main/java/networking/NanoHttpServer.java
@@ -40,8 +40,19 @@ public class NanoHttpServer extends  NanoHTTPD {
                 InputStream fs = m_FileChooserFragment.getContext().getContentResolver().openInputStream(m_FileChooserFragment.FileUri());
                 MimeTypeMap mime =  MimeTypeMap.getSingleton();
                 String extension = m_FileChooserFragment.FileUri().getPath().substring(m_FileChooserFragment.FileUri().getPath().lastIndexOf("."));
+                extension = extension.replace(".","");
                 String mimeType = mime.getMimeTypeFromExtension(extension);
-                return newChunkedResponse(Response.Status.OK, mimeType, fs);
+
+
+                if(session.getHeaders().get("range") != null)
+                {
+                    return getPartialResponse(fs,mimeType,session.getHeaders().get("range"));
+                } else {
+                    return getFullResponse(fs,mimeType);
+                }
+
+
+               // return newFixedLengthResponse(Response.Status.OK,mimeType,fs,fs.available());
             } catch (Exception e) {
                 Log.d("nanoHTTPD execpetion", e.getMessage());
             }
@@ -49,6 +60,58 @@ public class NanoHttpServer extends  NanoHTTPD {
         }
 
         return newFixedLengthResponse(msg + "</body></html>\n");
+    }
+
+    // handle non range requests
+    private  Response getFullResponse(InputStream fs, String mimeType)
+    {
+
+        try {
+            return newFixedLengthResponse(Response.Status.OK,mimeType,fs,fs.available());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    // handle range request
+    private  Response getPartialResponse(InputStream fs, String mimeType, String rangeHeader)
+    {
+        try
+        {
+            String rangeValue = rangeHeader.trim().substring("bytes=".length());
+            long fileLength = fs.available();
+            long start, end;
+            if (rangeValue.startsWith("-")) {
+                end = fileLength - 1;
+                start = fileLength - 1
+                        - Long.parseLong(rangeValue.substring("-".length()));
+            } else {
+                String[] range = rangeValue.split("-");
+                start = Long.parseLong(range[0]);
+                end = range.length > 1 ? Long.parseLong(range[1])
+                        : fileLength - 1;
+            }
+            if (end > fileLength - 1) {
+                end = fileLength - 1;
+            }
+            if (start <= end) {
+                long contentLength = end - start + 1;
+                //noinspection ResultOfMethodCallIgnored
+                fs.skip(start);
+                Response response = newChunkedResponse(Response.Status.PARTIAL_CONTENT, mimeType, fs);
+                response.addHeader("Content-Length", contentLength + "");
+                response.addHeader("Content-Range", "bytes " + start + "-" + end + "/" + fileLength);
+                response.addHeader("Content-Type", mimeType);
+                return response;
+            } else {
+                return newFixedLengthResponse(Response.Status.RANGE_NOT_SATISFIABLE, mimeType, rangeHeader);
+            }
+        } catch (Exception e)
+        {
+            throw new RuntimeException(e);
+        }
+
     }
 
 
