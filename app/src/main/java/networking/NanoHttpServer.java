76c7383e60d2ee83af8fecd60684434318398ff8
bahaaEldeen1999 <bahaaedeen1999@gmail.com>
2023-04-30 23:20:06 +0300

    fix large files, added name header, new way to get file size

--- app/src/main/java/networking/NanoHttpServer.java
+++ app/src/main/java/networking/NanoHttpServer.java
@@ -2,6 +2,8 @@ package networking;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.provider.OpenableColumns;
 import android.util.Log;
 import android.webkit.MimeTypeMap;
 
@@ -38,23 +40,35 @@ public class NanoHttpServer extends  NanoHTTPD {
         {
             try {
                 InputStream fs = m_FileChooserFragment.getContext().getContentResolver().openInputStream(m_FileChooserFragment.FileUri());
+                Cursor returnCursor = m_FileChooserFragment.getContext().getContentResolver().query(m_FileChooserFragment.FileUri(), null, null, null, null);
+                int sizeIndx = returnCursor.getColumnIndex(OpenableColumns.SIZE);
+                returnCursor.moveToFirst();
+                long size = returnCursor.getLong(sizeIndx);
+            //    File file = new File(m_FileChooserFragment.FileUri());
                 MimeTypeMap mime =  MimeTypeMap.getSingleton();
                 String extension = m_FileChooserFragment.FileUri().getPath().substring(m_FileChooserFragment.FileUri().getPath().lastIndexOf("."));
+                String fileName = m_FileChooserFragment.FileUri().getPath().substring(m_FileChooserFragment.FileUri().getPath().lastIndexOf("/"));
+                fileName = fileName.replace(' ','_');
                 extension = extension.replace(".","");
                 String mimeType = mime.getMimeTypeFromExtension(extension);
-
+                if(mimeType == null) mimeType = "application/octet-stream";
+                Log.d("MIMETYPE", mimeType);
+                Log.d("extension", extension);
+                Log.d("fs.avail", String.valueOf(fs.available()));
+                Log.d("fs size", String.valueOf(size));
 
                 if(session.getHeaders().get("range") != null)
                 {
-                    return getPartialResponse(fs,mimeType,session.getHeaders().get("range"));
+                    return getPartialResponse(fs,mimeType,session.getHeaders().get("range"),size,fileName);
                 } else {
-                    return getFullResponse(fs,mimeType);
+                    return getFullResponse(fs,mimeType,size,fileName);
                 }
 
 
                // return newFixedLengthResponse(Response.Status.OK,mimeType,fs,fs.available());
             } catch (Exception e) {
                 Log.d("nanoHTTPD execpetion", e.getMessage());
+                throw new RuntimeException(e);
             }
 
         }
@@ -63,24 +77,29 @@ public class NanoHttpServer extends  NanoHTTPD {
     }
 
     // handle non range requests
-    private  Response getFullResponse(InputStream fs, String mimeType)
+    private  Response getFullResponse(InputStream fs, String mimeType, long size, String fileName)
     {
 
         try {
-            return newFixedLengthResponse(Response.Status.OK,mimeType,fs,fs.available());
-        } catch (IOException e) {
+            Response response = newFixedLengthResponse(Response.Status.OK,mimeType,fs,size);
+            response.addHeader("Content-Length", size + "");
+            response.addHeader("Content-Range", "bytes " + 0 + "-" + size + "/" + size);
+            response.addHeader("Content-Type", mimeType);
+            response.addHeader("Content-Disposition", "attachment; filename=\""+fileName+"\"");
+            return response;
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
 
     // handle range request
-    private  Response getPartialResponse(InputStream fs, String mimeType, String rangeHeader)
+    private  Response getPartialResponse(InputStream fs, String mimeType, String rangeHeader, long size, String fileName)
     {
         try
         {
             String rangeValue = rangeHeader.trim().substring("bytes=".length());
-            long fileLength = fs.available();
+            long fileLength = size;
             long start, end;
             if (rangeValue.startsWith("-")) {
                 end = fileLength - 1;
@@ -103,6 +122,7 @@ public class NanoHttpServer extends  NanoHTTPD {
                 response.addHeader("Content-Length", contentLength + "");
                 response.addHeader("Content-Range", "bytes " + start + "-" + end + "/" + fileLength);
                 response.addHeader("Content-Type", mimeType);
+                response.addHeader("Content-Disposition", "attachment; filename=\""+fileName+"\"");
                 return response;
             } else {
                 return newFixedLengthResponse(Response.Status.RANGE_NOT_SATISFIABLE, mimeType, rangeHeader);
